3b6b68a748e168829944a2b1f4da9da72c6e3aa0
Luke Tillman <luke.tillman@gmail.com>
2014-08-16 16:41:27 -0600

    Add log4net logging along with a trace appender for Azure diagnostics

--- src/KillrVideo/Global.asax.cs
+++ src/KillrVideo/Global.asax.cs
@@ -1,16 +1,26 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Castle.Windsor;
+using log4net;
+using log4net.Config;
 
 namespace KillrVideo
 {
     public class MvcApplication : HttpApplication
     {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof (MvcApplication));
+
         private static IWindsorContainer _container;
 
         protected void Application_Start()
         {
+            // Bootstrap log4net logging
+            XmlConfigurator.Configure();
+
+            Logger.Info("KillrVideo is starting");
+
             // Bootstrap Windsor container and tell MVC to use Windsor to create IController instances
             _container = WindsorConfig.CreateContainer();
             ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(_container.Kernel));
@@ -23,9 +33,20 @@ namespace KillrVideo
 
         protected void Application_End()
         {
+            Logger.Info("KillrVideo is stopping");
+
             // Properly clean-up the container on app end
             if (_container != null)
                 _container.Dispose();
+        }
+
+        protected void Application_Error(Object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+            if (ex == null)
+                return;
+
+            Logger.Error(string.Empty, ex);
         }
     }
 }
