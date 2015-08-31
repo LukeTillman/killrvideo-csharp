990f9c283e8abef9d9228c3867830f746ba73544
Luke Tillman <luke.tillman@gmail.com>
2015-08-31 14:48:02 -0600

    Choose the correct suggested videos implementation based on whether DSE is enabled in the config or not

--- src/KillrVideo.SuggestedVideos/SuggestedVideosServiceWindsorInstaller.cs
+++ src/KillrVideo.SuggestedVideos/SuggestedVideosServiceWindsorInstaller.cs
@@ -1,6 +1,9 @@
-﻿using Castle.MicroKernel.Registration;
+﻿using System;
+using Castle.Facilities.TypedFactory;
+using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
+using KillrVideo.SuggestedVideos.SuggestionImpl;
 
 namespace KillrVideo.SuggestedVideos
 {
@@ -12,8 +15,24 @@ namespace KillrVideo.SuggestedVideos
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
             container.Register(
-                // Most components
-                Classes.FromThisAssembly().Pick().WithServiceFirstInterface().LifestyleTransient()
+                // Most components (except suggested videos implementations namespace)
+                Classes.FromThisAssembly()
+                       .Where(t => t.Namespace != typeof (SuggestVideosByTag).Namespace)
+                       .WithServiceFirstInterface()
+                       .LifestyleTransient(),
+
+                // Suggested videos implementations namespace
+                Classes.FromThisAssembly()
+                       .InSameNamespaceAs<SuggestVideosByTag>()
+                       .WithServiceSelf()
+                       .LifestyleTransient()
+                       .ConfigureFor<SuggestedVideosComponentSelector>(c => c.LifestyleSingleton()),
+
+                // Factory method for selecting a suggested videos service using the custom selector
+                Component.For<Func<ISuggestVideos>>().AsFactory(c => c.SelectedWith<SuggestedVideosComponentSelector>()).LifestyleSingleton(),
+
+                // When resolving the search service, use the factory method
+                Component.For<ISuggestVideos>().UsingFactoryMethod(k => k.Resolve<Func<ISuggestVideos>>().Invoke()).LifestyleTransient()
             );
         }
     }
---
+++ src/KillrVideo.SuggestedVideos/SuggestionImpl/SuggestedVideosComponentSelector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+using Castle.Facilities.TypedFactory;
+using KillrVideo.Utils.Configuration;
+
+namespace KillrVideo.SuggestedVideos.SuggestionImpl
+{
+    /// <summary>
+    /// A component selector for Castle Windsor that decides which Video Suggestions implementation to use.
+    /// </summary>
+    public class SuggestedVideosComponentSelector : DefaultTypedFactoryComponentSelector
+    {
+        private const string EnterpriseSearchConfigKey = "EnterpriseSearchEnabled";
+
+        private static readonly Type EnterpriseSearchType = typeof (DataStaxEnterpriseSuggestedVideos);
+        private static readonly Type TagSearchType = typeof (SuggestVideosByTag);
+
+        private readonly IGetEnvironmentConfiguration _configRetriever;
+        private readonly Lazy<bool> _useEnterpriseSearch;
+
+        public SuggestedVideosComponentSelector(IGetEnvironmentConfiguration configRetriever)
+        {
+            if (configRetriever == null) throw new ArgumentNullException("configRetriever");
+            _configRetriever = configRetriever;
+            _useEnterpriseSearch = new Lazy<bool>(CanUseEnterpriseSearch);
+        }
+
+        protected override Type GetComponentType(MethodInfo method, object[] arguments)
+        {
+            return _useEnterpriseSearch.Value ? EnterpriseSearchType : TagSearchType;
+        }
+
+        private bool CanUseEnterpriseSearch()
+        {
+            try
+            {
+                // Use a flag in the configuration settings to determine whether to use DSE search
+                string enabled = _configRetriever.GetSetting(EnterpriseSearchConfigKey);
+                return bool.Parse(enabled);
+            }
+            catch (Exception)
+            {
+                // If something goes wrong, just assume it's not available
+                return false;
+            }
+        }
+    }
+}
