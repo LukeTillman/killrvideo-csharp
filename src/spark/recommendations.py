efcd19e0717a693bbddb0b2fc30ede477ab4383f
Jon Haddad <jon@jonhaddad.com>
2015-09-08 23:18:55 -0700

    working recommendation engine.  kind of inefficient, but working

--- src/spark/recommendations.py
+++ src/spark/recommendations.py
@@ -42,5 +42,16 @@ model.save(sc, "/tmp/recommendations")
 
 for user in user_map.collect():
     products = model.recommendProducts(user.iu, 30)
+    products = sql.createDataFrame(products)
+
+    recs = products.join(video_map, video_map.iv == products.product).\
+            join(user_map, user_map.iu == products.user).\
+            select(user_map.u.alias("userid"), video_map.v.alias("videoid"), "rating")
+
+    recs.write.format("org.apache.spark.sql.cassandra").\
+        options(keyspace="killrvideo", table="video_recommendations").\
+        save(mode="append")
+
+    print products
     userid = user.u
     print userid
