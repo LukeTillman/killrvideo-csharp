41881651c1f6761cae1a06e7230d235136a6dee3
Jon Haddad <jon@jonhaddad.com>
2015-09-11 14:04:29 -0700

    fixed distinct lists for uniqueid

--- src/spark/recommendations.py
+++ src/spark/recommendations.py
@@ -18,17 +18,21 @@ sc = SparkContext()
 sql = SQLContext(sc)
 
 ratings = sql.read.format("org.apache.spark.sql.cassandra").load(keyspace="killrvideo", table="video_ratings_by_user")
+
+# we have to cast our UUIDs to strings because spark doesn't know what to do with them in UUID form
 ratings = ratings.select(ratings.userid.cast("string").alias("userid"),
                    ratings.videoid.cast("string").alias("videoid"),
                    ratings.rating).cache()
 
 # the ML libraries require integers, so we need to create keys for the users & videos temporarily
-user_ids = ratings.rdd.zipWithUniqueId()
+user_ids = ratings.select("userid").distinct().rdd.zipWithUniqueId()
 user_map = user_ids.map(lambda (x, y): Row(u=x.userid, iu=y)).toDF().cache()
 
-video_ids = ratings.distinct().rdd.zipWithUniqueId().cache()
+# same as above - this is a UUID/int mapping
+video_ids = ratings.select("videoid").distinct().rdd.zipWithUniqueId().cache()
 video_map = video_ids.map(lambda (x, y): Row(v=x.videoid, iv=y)).toDF().cache()
 
+# lets join the mapping tables to the original data, and swap our UUIDS for the int ids
 
 training_data = ratings.join(video_map, ratings.videoid == video_map.v).\
                 join(user_map, ratings.userid == user_map.u).\
@@ -38,8 +42,12 @@ training_data = ratings.join(video_map, ratings.videoid == video_map.v).\
 
 model = ALS.train(training_data, 10)
 
-model.save(sc, "/tmp/recommendations")
+# later we're going to want to save the model and the 2 mappings
+# but not just yet
+# model.save(sc, "/tmp/recommendations")
 
+# TODO only get recommendations for users who have less than 20
+# sadly, we have to do this in the driver.  the model can't be distributed
 for user in user_map.collect():
     products = model.recommendProducts(user.iu, 30)
     products = sql.createDataFrame(products)
