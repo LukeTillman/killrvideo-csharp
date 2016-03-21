231c5c51f32262477cd9eb8d444eb4f4a9ee7a08
Luke Tillman <luke.tillman@gmail.com>
2016-03-21 17:26:36 -0600

    Change uploads service over to implementing the grpc interface
    - This is another one that needs a bit of rethinking now that the service is in a separate process (what to do about the bus?)

--- src/KillrVideo.Uploads/UploadsService.cs
+++ src/KillrVideo.Uploads/UploadsService.cs
@@ -2,7 +2,8 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Cassandra;
-using KillrVideo.Uploads.Dtos;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using KillrVideo.Utils;
 using Nimbus;
 
@@ -12,17 +13,17 @@ namespace KillrVideo.Uploads
     /// An implementation of the video uploads service that stores data in Cassandra and uses a message bus for sending commands to
     /// backend processes and publishing events.
     /// </summary>
-    public class UploadsService : IUploadsService
+    public class UploadsServiceImpl : UploadsService.IUploadsService
     {
         private readonly ISession _session;
         private readonly TaskCache<string, PreparedStatement> _statementCache;
         private readonly IBus _bus;
 
-        public UploadsService(ISession session, TaskCache<string, PreparedStatement> statementCache, IBus bus)
+        public UploadsServiceImpl(ISession session, TaskCache<string, PreparedStatement> statementCache, IBus bus)
         {
-            if (session == null) throw new ArgumentNullException("session");
-            if (statementCache == null) throw new ArgumentNullException("statementCache");
-            if (bus == null) throw new ArgumentNullException("bus");
+            if (session == null) throw new ArgumentNullException(nameof(session));
+            if (statementCache == null) throw new ArgumentNullException(nameof(statementCache));
+            if (bus == null) throw new ArgumentNullException(nameof(bus));
             _session = session;
             _statementCache = statementCache;
             _bus = bus;
@@ -31,40 +32,37 @@ namespace KillrVideo.Uploads
         /// <summary>
         /// Generates upload destinations for users to upload videos.
         /// </summary>
-        public Task<UploadDestination> GenerateUploadDestination(GenerateUploadDestination uploadDestination)
+        public Task<GetUploadDestinationResponse> GetUploadDestination(GetUploadDestinationRequest request, ServerCallContext context)
         {
             // Do request/response via the bus
-            return _bus.Request(uploadDestination);
+            return _bus.Request(request);
         }
 
         /// <summary>
         /// Marks an upload as complete.
         /// </summary>
-        public Task MarkUploadComplete(MarkUploadComplete upload)
+        public Task<MarkUploadCompleteResponse> MarkUploadComplete(MarkUploadCompleteRequest request, ServerCallContext context)
         {
             // Send command via the bus
-            return _bus.Send(upload);
+            return _bus.Send(request);
         }
 
         /// <summary>
         /// Gets the status of an uploaded video's encoding job by the video Id.
         /// </summary>
-        public async Task<EncodingJobProgress> GetStatusForVideo(Guid videoId)
+        public async Task<GetStatusOfVideoResponse> GetStatusOfVideo(GetStatusOfVideoRequest request, ServerCallContext context)
         {
             PreparedStatement preparedStatement =
                 await _statementCache.NoContext.GetOrAddAsync("SELECT newstate, status_date FROM encoding_job_notifications WHERE videoid = ? LIMIT 1");
-            RowSet rows = await _session.ExecuteAsync(preparedStatement.Bind(videoId)).ConfigureAwait(false);
-            return MapRowToEncodingJobProgress(rows.SingleOrDefault());
-        }
+            RowSet rows = await _session.ExecuteAsync(preparedStatement.Bind(request.VideoId.ToGuid())).ConfigureAwait(false);
+            Row row = rows.SingleOrDefault();
+            if (row == null)
+                return new GetStatusOfVideoResponse();
 
-        private static EncodingJobProgress MapRowToEncodingJobProgress(Row row)
-        {
-            if (row == null) return null;
-
-            return new EncodingJobProgress
+            return new GetStatusOfVideoResponse
             {
                 CurrentState = row.GetValue<string>("newstate"),
-                StatusDate = row.GetValue<DateTimeOffset>("status_date")
+                StatusDate = row.GetValue<DateTimeOffset>("status_date").ToTimestamp()
             };
         }
     }
