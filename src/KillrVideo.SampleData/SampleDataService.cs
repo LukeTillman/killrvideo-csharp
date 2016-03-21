c7fe7116abfa206028188a66ced08502a5bfb312
Luke Tillman <luke.tillman@gmail.com>
2016-03-21 15:55:26 -0600

    Update sample data service to implement grpc interface
    - This definitely will need some work now that we're standing this up in its own server (maybe remove the bus completely?)

--- src/KillrVideo.SampleData/SampleDataService.cs
+++ src/KillrVideo.SampleData/SampleDataService.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
-using KillrVideo.SampleData.Dtos;
+using Grpc.Core;
 using Nimbus;
 
 namespace KillrVideo.SampleData
@@ -8,60 +8,60 @@ namespace KillrVideo.SampleData
     /// <summary>
     /// Implementation of sample data service that simply sends commands on the bus to a backend worker.
     /// </summary>
-    public class SampleDataService : ISampleDataService
+    public class SampleDataServiceImpl : SampleDataService.ISampleDataService
     {
         private readonly IBus _bus;
 
-        public SampleDataService(IBus bus)
+        public SampleDataServiceImpl(IBus bus)
         {
-            if (bus == null) throw new ArgumentNullException("bus");
+            if (bus == null) throw new ArgumentNullException(nameof(bus));
             _bus = bus;
         }
 
         /// <summary>
         /// Adds sample comments to the site.
         /// </summary>
-        public Task AddSampleComments(AddSampleComments comments)
+        public Task<AddSampleCommentsResponse> AddSampleComments(AddSampleCommentsRequest request, ServerCallContext context)
         {
-            return _bus.Send(comments);
+            return _bus.Send(request);
         }
 
         /// <summary>
         /// Adds sample video ratings to the site.
         /// </summary>
-        public Task AddSampleRatings(AddSampleRatings ratings)
+        public Task<AddSampleRatingsResponse> AddSampleRatings(AddSampleRatingsRequest request, ServerCallContext context)
         {
-            return _bus.Send(ratings);
+            return _bus.Send(request);
         }
 
         /// <summary>
         /// Adds sample users to the site.
         /// </summary>
-        public Task AddSampleUsers(AddSampleUsers users)
+        public Task<AddSampleUsersResponse> AddSampleUsers(AddSampleUsersRequest request, ServerCallContext context)
         {
-            return _bus.Send(users);
+            return _bus.Send(request);
         }
 
         /// <summary>
         /// Adds sample video views to the site.
         /// </summary>
-        public Task AddSampleVideoViews(AddSampleVideoViews views)
+        public Task<AddSampleVideoViewsResponse> AddSampleVideoViews(AddSampleVideoViewsRequest request, ServerCallContext context)
         {
-            return _bus.Send(views);
+            return _bus.Send(request);
         }
 
         /// <summary>
         /// Adds sample YouTube videos to the site.
         /// </summary>
-        public Task AddSampleYouTubeVideos(AddSampleYouTubeVideos videos)
+        public Task<AddSampleYouTubeVideosResponse> AddSampleYouTubeVideos(AddSampleYouTubeVideosRequest request, ServerCallContext context)
         {
-            return _bus.Send(videos);
+            return _bus.Send(request);
         }
 
         /// <summary>
         /// Triggers a refresh of the YouTube sample video sources.
         /// </summary>
-        public Task RefreshYouTubeSources(RefreshYouTubeSources refresh)
+        public Task<RefreshYouTubeSourcesResponse> RefreshYouTubeSources(RefreshYouTubeSourcesRequest request, ServerCallContext context)
         {
             return _bus.Send(refresh);
         }
