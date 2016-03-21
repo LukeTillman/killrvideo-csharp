6fd43f7087ea81f27601816fae48d67243a81306
Luke Tillman <luke.tillman@gmail.com>
2016-03-21 15:14:38 -0600

    Add some helper methods for converting to/from common protobuf types

---
+++ src/KillrVideo.Protobuf/UuidExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace KillrVideo.Protobuf
+{
+    /// <summary>
+    /// Helper extension methods for dealing with Uuids and TimeUuids.
+    /// </summary>
+    public static class UuidExtensions
+    {
+        /// <summary>
+        /// Converts a Guid to a Uuid.
+        /// </summary>
+        public static Uuid ToUuid(this Guid value)
+        {
+            return new Uuid { Value = value.ToString() };
+        }
+
+        /// <summary>
+        /// Converts a Guid to a TimeUuid.
+        /// </summary>
+        public static TimeUuid ToTimeUuid(this Guid value)
+        {
+            return new TimeUuid { Value = value.ToString() };
+        }
+    }
+}
