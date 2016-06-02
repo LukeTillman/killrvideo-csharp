37e0477312d2ee54db56813c970eb1083bc9299a
Luke Tillman <luke.tillman@gmail.com>
2016-06-02 09:27:20 -0600

    Make creating the .env file for docker compose part of the build, add an empty docker-compose.yaml file

--- build/CreateEnvFile.ps1
+++ build/CreateEnvFile.ps1
@@ -1,19 +1,30 @@
 <#
   .DESCRIPTION
-  Create a docker compose .env file
+  Create a docker compose .env file at the path specified.
+
+  .PARAMETER FilePath
+  The path to the file to create.
+
+  .PARAMETER Force
+  Tells the script to create the file even if it already exists
 #>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $FilePath
+)
+
 $scriptPath = Split-Path -parent $PSCommandPath
-$envFilePath = "$scriptPath\..\.env"
 $getEnvCommand = Resolve-Path "$scriptPath\..\lib\killrvideo-docker-common\get-environment.ps1"
 
 # Base environment variables to be written
-$dockerEnv = @("COMPOSE_PROJECT_NAME=killrvideocsharp", "COMPOSE_FILE=.\lib\killrvideo-docker-common\docker-compose.yaml")
+$dockerEnv = @("COMPOSE_PROJECT_NAME=killrvideocsharp")
 
 # Get the docker environment using the common script and add to array
 & "$getEnvCommand" |% { $dockerEnv += $_ }
 
 # We have to use .NET to do this so it gets written as UTF-8 without the BOM
 $Utf8NoBom = New-Object System.Text.UTF8Encoding($false)
-[System.IO.File]::WriteAllLines($envFilePath, $dockerEnv, $Utf8NoBom)
-$envFilePath = Resolve-Path $envFilePath
-Write-Host "Environment file written to $envFilePath" 
+[System.IO.File]::WriteAllLines($FilePath, $dockerEnv, $Utf8NoBom)
+Write-Host "Environment file written to $FilePath"
