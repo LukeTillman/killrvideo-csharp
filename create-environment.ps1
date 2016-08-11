31eb4c8999bf552d6207f578fbd07984f4cd00b6
Luke Tillman <luke.tillman@gmail.com>
2016-08-11 14:45:24 -0600

    Squashed 'lib/killrvideo-docker-common/' changes from 0bc2e46..0c67c94
    0c67c94 Add powershell script for creating a .env file
    
    git-subtree-dir: lib/killrvideo-docker-common
    git-subtree-split: 0c67c9442eb3873625ff39407e5a2c8a6b07e24c

---
+++ create-environment.ps1
@@ -0,0 +1,68 @@
+<#
+    .DESCRIPTION
+    Gets the environment variables needed to run the Killrvideo docker-compose commands, then writes
+    them to a .env file in the working directory or the directory specified by the -Path switch.
+
+    .PARAMETER Path
+    The path to create the environment file. Defaults to the current working directory.
+
+    .PARAMETER FileName
+    The name of the environment file. Defaults to ".env".
+
+    .PARAMETER ProjectName
+    The COMPOSE_PROJECT_NAME value to use. Defaults to "killrvideo".
+
+    .PARAMETER Force
+    Switch to force creation of the file (i.e. overwrite it) if it already exists.
+#>
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory=$false)]
+    [string]
+    $Path = $((Resolve-Path .\).Path),
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $FileName = '.env',
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $ProjectName = 'killrvideo',
+
+    [Parameter(Mandatory=$false)]
+    [switch]
+    $Force
+)
+
+$ErrorActionPreference = 'stop'
+
+# Make sure we have an absolute path
+if ([System.IO.Path]::IsPathRooted($Path) -eq $false) {
+    $cwd = (Resolve-Path .\).Path
+    $Path = Join-Path $cwd $Path
+}
+
+# Path to the file and does it exist
+$envFilePath = Join-Path $Path $FileName
+if ((Test-Path $envFilePath) -and ($Force -eq $false)) {
+    Write-Host "Environment file $(Resolve-Path $envFilePath) already exists, will not overwrite"
+    Exit
+}
+
+# Base environment variables
+$dockerEnv = @("COMPOSE_PROJECT_NAME=$ProjectName")
+
+# Get path to the get-environment script and run it, adding each value to the env array
+$scriptPath = Split-Path -parent $PSCommandPath
+$getEnvCommand = Resolve-Path "$scriptPath\get-environment.ps1"
+& "$getEnvCommand" |% { $dockerEnv += $_ }
+
+# Make sure the path exists for the file
+if ((Test-Path $Path) -eq $false) {
+    New-Item -Path $Path -Type Directory | Out-Null
+}
+
+# Write the file (have to use the .NET API here because we need UTF-8 WITHOUT the BOM)
+$Utf8NoBom = New-Object System.Text.UTF8Encoding($false)
+[System.IO.File]::WriteAllLines($envFilePath, $dockerEnv, $Utf8NoBom)
+Write-Host "Environment file written to $(Resolve-Path $envFilePath)"
