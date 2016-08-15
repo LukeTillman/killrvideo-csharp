6eb5094e3654e46cb6c55c127e0294ca0ca3bf86
Luke Tillman <luke.tillman@gmail.com>
2016-08-15 13:32:47 -0600

    Setup docker can now just use the PS script for generating the .env file
    - No need to append COMPOSE_FILE since the common script now handles that

--- setup-docker.bat
+++ setup-docker.bat
@@ -3,16 +3,6 @@
 REM Make sure we have a .env file
 powershell -NonInteractive -ExecutionPolicy Unrestricted -File .\lib\killrvideo-docker-common\create-environment.ps1 -Force
 
-REM Capture the current code page, then switch to UTF-8
-for /f "tokens=2 delims=:." %%x in ('chcp') do set cp=%%x
-chcp 65001 > NUL
-
-REM Tell docker-compose to use our file, plus the one in killrvideo-docker-common
-echo COMPOSE_FILE=.\lib\killrvideo-docker-common\docker-compose.yaml;.\docker-compose.yaml >> .env
-
-REM Change back to original code page
-chcp %cp% > NUL
-
 echo.
 echo Pulling all docker dependencies
 
