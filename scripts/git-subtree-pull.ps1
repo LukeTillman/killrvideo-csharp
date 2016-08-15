a9a4d0e90c3996c5763e5c00640fc5e90fc7ab3e
Luke Tillman <luke.tillman@gmail.com>
2016-08-15 13:30:39 -0600

    Add killrvideo-data to subtree pull script

--- scripts/git-subtree-pull.ps1
+++ scripts/git-subtree-pull.ps1
@@ -5,3 +5,4 @@
 
 git subtree pull --prefix lib/killrvideo-docker-common git@github.com:KillrVideo/killrvideo-docker-common.git master --squash
 git subtree pull --prefix lib/killrvideo-service-protos git@github.com:KillrVideo/killrvideo-service-protos.git master --squash
+git subtree pull --prefix lib/killrvideo-data git@github.com:KillrVideo/killrvideo-data.git master --squash
