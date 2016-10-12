812c36724caeea2825b005b25474a79b464e4856
Luke Tillman <luke.tillman@gmail.com>
2016-10-12 09:39:39 -0600

    Remove the killrvideo-data subtree
    - We no longer need it since the schema/search bootstrapping is now done by the killrvideo-dse docker image

--- lib/killrvideo-data/README.md
+++
@@ -1,5 +0,0 @@
-# KillrVideo Data
-
-This repository contains the schema and other database-related scripts for [KillrVideo][killrvideo].
-
-[killrvideo]: https://killrvideo.github.io/
--- scripts/git-subtree-pull.ps1
+++ scripts/git-subtree-pull.ps1
@@ -5,4 +5,3 @@
 
 git subtree pull --prefix lib/killrvideo-docker-common git@github.com:KillrVideo/killrvideo-docker-common.git master --squash
 git subtree pull --prefix lib/killrvideo-service-protos git@github.com:KillrVideo/killrvideo-service-protos.git master --squash
-git subtree pull --prefix lib/killrvideo-data git@github.com:KillrVideo/killrvideo-data.git master --squash
