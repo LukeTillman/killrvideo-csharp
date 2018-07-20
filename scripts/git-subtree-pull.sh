ca664987094717ec55189c2ee4b49438321842e5
Jeffrey Carpenter <jeffreyscarpenter@cox.net>
2018-07-20 10:06:58 +0200

    adding subtree pull script for Mac

---
+++ scripts/git-subtree-pull.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Pull the latest from projects that are git subtrees
+git subtree pull --prefix lib/killrvideo-docker-common git@github.com:KillrVideo/killrvideo-docker-common.git master --squash
+git subtree pull --prefix lib/killrvideo-service-protos git@github.com:KillrVideo/killrvideo-service-protos.git master --squash
+
