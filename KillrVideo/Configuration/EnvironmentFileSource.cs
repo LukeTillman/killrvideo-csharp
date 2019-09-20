c4739b4696f6047ae55155da9bdf12086fc87940
Jeffrey Carpenter <jeffreyscarpenter@cox.net>
2019-09-20 14:02:22 -0700

    added base KillrVideo project code from previous version

---
+++ KillrVideo/Configuration/EnvironmentFileSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace KillrVideo.Configuration
+{
+    /// <summary>
+    /// Configuration source for reading parameters from a .env file.
+    /// </summary>
+    public class EnvironmentFileSource : IConfigurationSource
+    {
+        public IDictionary<string, string> Mappings { get; set; }
+
+        public EnvironmentFileSource(IDictionary<string, string> mappings)
+        {
+            Mappings = mappings;
+            if (mappings == null) throw new ArgumentNullException(nameof(mappings));
+        }
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new EnvironmentFileProvider(Mappings);
+        }
+    }
+}
---
+++ KillrVideo/ServiceDiscovery/EtcdClientFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Composition;
+using DryIocAttributes;
+using EtcdNet;
+
+namespace KillrVideo.ServiceDiscovery
+{
+    /// <summary>
+    /// Static factory class for creating an Etcd client.
+    /// </summary>
+    [Export, AsFactory]
+    public static class EtcdClientFactory
+    {
+        [Export]
+        public static EtcdClient CreateEtcdClient(EtcdOptions options)
+        {
+            var opts = new EtcdClientOpitions()
+            {
+                Urls = new[] {$"http://{options.IP}:{options.Port}"}
+            };
+            return new EtcdClient(opts);
+        }
+    }
+}
